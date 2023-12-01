593be99f0b79bb7ab46ddb2fb45921e47b513b2d
Slava Rochlin <slavarik2000@gmail.com>
2023-12-01 15:51:27 -0500

    comment out cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "slavarik2000"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
